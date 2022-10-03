b7ebfad6b982a18bcf75a898df1893309097aa5b
sunilmsd007 <sunilmsd007@gmail.com>
2022-10-03 19:47:40 +0530

    Modified: test case to return sad when message given in constructor

--- src/test/java/sample/AnalyseMoodTest.java
+++ src/test/java/sample/AnalyseMoodTest.java
@@ -7,15 +7,9 @@ public class AnalyseMoodTest {
 
 	@Test
 	public void givenMessageShouldReturnSad() {
-		AnalyseMood analyse = new AnalyseMood();
-		String actualResult = analyse.moodAnalyser("I am in Sad Mood");
+		AnalyseMood analyse = new AnalyseMood("I am in Sad Mood");
+		String actualResult = analyse.moodAnalyser();
 		Assert.assertEquals("Sad", actualResult);
 	}
 
-	@Test
-	public void givenMessageContainAnyShouldReturnHappy() {
-		AnalyseMood analyse = new AnalyseMood();
-		String actualResult = analyse.moodAnalyser("I am in any Mood");
-		Assert.assertEquals("Happy", actualResult);
-	}
 }
