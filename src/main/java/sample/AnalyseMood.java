3af19201f0d4f848904384113a26bc480d57ae7c
sunilmsd007 <sunilmsd007@gmail.com>
2022-10-03 19:58:28 +0530

    Modified: handle exception if user provides invalid mood

--- src/main/java/sample/AnalyseMood.java
+++ src/main/java/sample/AnalyseMood.java
@@ -10,19 +10,24 @@ public class AnalyseMood {
 
 	// method to analyse mood by reading string
 	public String moodAnalyser() {
-		if (message.contains("Happy")) {
+		try {
+			if (message.contains("Happy")) {
+				return "Happy";
+			} else if (message.contains("Sad")) {
+				return "Sad";
+			} else if (message.contains("any")) {
+				return "Happy";
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			// when user provides null input, handling the exception by returning Happy
 			return "Happy";
-		} else if (message.contains("Sad")) {
-			return "Sad";
-		} else if (message.contains("any")) {
-			return "Happy";
-		} else {
-			return null;
 		}
 	}
 
 	public static void main(String[] args) {
-		AnalyseMood analyse = new AnalyseMood("Happy");
+		AnalyseMood analyse = new AnalyseMood(null);
 		String moodHappy = analyse.moodAnalyser();
 		System.out.println(moodHappy);
 
